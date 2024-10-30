d99ec14c5c187a0fb3ee94417e1e59860ab90dd8
vivianepavaoo <146456646+vivianepavaoo@users.noreply.github.com>
2024-10-30 10:20:36 -0300

    30/10

--- inde/app.js
+++ inde/app.js
@@ -2,7 +2,5 @@ function criacartao(categoria, pergunta, resposta) {
     let container = document.getElementById(container);
     let cartao = document.createElement(artlice)
     cartao.classname = cartao;
-            
-        }
-    }
-}
+
+    cartao.inneHTMl=
