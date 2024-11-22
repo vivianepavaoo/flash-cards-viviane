d4c9a4758842f6445a45dcb6edc9bb5ffbcd20a1
vivianepavaoo <146456646+vivianepavaoo@users.noreply.github.com>
2024-11-22 11:46:38 -0300

    22/11

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'Meio ambiente',
+    'Quais são os principais fatores que contribuem para poluicão?',
+    'R:Os processos industriais e de geração de energia, os veículos automotores e as queimadas'
+)
+
+criaCartao(
+    'Meio ambiente',
+    'Quai são os efeitos da poluição da água na saúde humana e na vida aquática?',
+    'R:A ingestão de água contaminada po9de causae uma série de doenças, incluindo gastrioenterite, hepatite, entre outros'
+)
+
+criaCartao(
+    'Meio ambiente',
+    'Como o aquecimento global esta alterando os padrões climatica e os habitats naturais?',
+    'R:O aquecimento global agrava os periodos de seca em regiões onde a falta de água já é comu  e eleva a um risco maior de secas'
+)
+
+criaCartao(
+    'Meio ambiente',
+    'O que causa impactos ambientais?',
+    'R:No planeta são a numeração, a agricultura, a exploração florestal'
+)
+
