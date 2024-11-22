cad2957773000a2f950010e8027d282367794da1
vivianepavaoo <146456646+vivianepavaoo@users.noreply.github.com>
2024-11-22 11:28:23 -0300

    22/11

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function criaCartao(categoria, pergunta, resposta) {
     let container = document.getElementById('container');
     let cartao = document.createElement('article');
     cartao.className = 'cartao';
-    
+
     cartao.innerHTML = `
     <div class="cartao__conteudo">
     <h3>${categoria}</h3>
@@ -14,16 +14,16 @@ function criaCartao(categoria, pergunta, resposta) {
     </div>
     </div>
     `
-    
+
     let respostaEstaVisivel = false;
-    
+
     function viraCartao() {
-    respostaEstaVisivel = !respostaEstaVisivel;
-    cartao.classList.toggle('active', respostaEstaVisivel);
+        respostaEstaVisivel = !respostaEstaVisivel;
+        cartao.classList.toggle('active', respostaEstaVisivel);
     }
     cartao.addEventListener('click', viraCartao);
-    
-    
+
+
     container.appendChild(cartao);
-    
-    }
+
+}
